3ce102ccd2c2d09b55faa94e745d41b5721196fc
Andravi <andravi75@gmail.com>
2023-03-15 19:04:22 -0300

    fix: change the functions uses

--- teste_jikan_ga_nai.py
+++ teste_jikan_ga_nai.py
@@ -1,14 +1,13 @@
 import json
 import requests
 
-
 def get_request(url_args: str) -> dict:
     """ Faz o request e retorna o json_data
     :param - url_args: argumentos colocados na url para pegar o json
     :return - Json resposta na forma de Dict
     """
     try:
-        response = requests.get(f"https://api.jikan.moe/v4/{url_args}", timeout=10)
+        response = requests.get(f"https://api.jikan.moe/v4/{url_args}", timeout=None)
 
         json_data = json.loads(response.text)
         return json_data
@@ -18,7 +17,7 @@ def get_request(url_args: str) -> dict:
         return None
 
 
-def get_day(id_mal: int):
+def get_day(id_mal: int): # pode fazer a conversão para o dia e horas do Brasil
     """ diz quando o anime com o ID informado lança novos episodeos
     :param - id_mal
     :return - None
@@ -32,21 +31,28 @@ def get_day(id_mal: int):
     print(f"\nO anime {nome} lança um episodio novo as {time} de {day}\n")
 
 
-def lista_do_dia(day:str):
-    """ Diz os animes que lançam no dia informado"""
+def lista_do_dia(day:str) -> list: # Posso acabar usando muito essa
+    """ Diz os animes que lançam no dia informado
+    
+    return: [dict{titulo, image, gerenos, dia e hora no brasil}, ...]"""
+    anime_list = []
     current_page = 1
     data = get_request(f"seasons/now?page={current_page}")
 
     while data["pagination"]["last_visible_page"] >= current_page:
-        # print(current_page)
-        for title in data["data"]:
-            if title["broadcast"]["day"] == day:
-                print(title["title"], f"id={title['mal_id']},\n")
+        for anime in data["data"]:
+            if anime["broadcast"]["day"] == day:
+                anime_dict = {}
+                anime_dict["nome"] = anime["title"]
+                anime_dict["image"] = anime["images"]["jpg"]["image_url"]
+                anime_list.append(anime_dict)
+                # anime_list["novo_ep"] = get_day(anime["broadcast"])
 
         current_page += 1
         data = get_request(f"seasons/now?page={current_page}")
+    return anime_list # pode retornar uma lista de dicionarios com titulo, imagem e sinopse # To começando a gostar dessa ideia
 
 
-
-get_day(id_mal=1)
-#lista_do_dia("Mondays")
+#get_day(id_mal=50287)
+print(lista_do_dia("Mondays"))
+#print(json.dumps(get_request("seasons/now"), indent=4))
