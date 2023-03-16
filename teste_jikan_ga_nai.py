117f5064c4381aff77ccfaa426d17ce1e47c5212
Andravi <andravi75@gmail.com>
2023-03-16 19:28:21 -0300

    feat: time converter implemented

--- teste_jikan_ga_nai.py
+++ teste_jikan_ga_nai.py
@@ -17,18 +17,63 @@ def get_request(url_args: str) -> dict:
         return None
 
 
-def get_day(id_mal: int): # pode fazer a conversão para o dia e horas do Brasil
+
+def convert_day(days: str, mod: bool = False) -> str:
+    """ Converte os dias em inglês para portugues e modifica caso necessário """
+
+    week = ["Sundays", "Mondays", "Tuesday", "Wednesdays", "Thursdays", "Fridays", "Saturdays"]
+    day_num = (week.index(days) - 1 if mod else week.index(days))
+
+    if week[day_num] == "Sundays":
+        return "Domingos"
+
+    elif week[day_num] == "Mondays":
+        return "Segundas"
+
+    elif week[day_num] == "Tuesday":
+        return "Terças"
+
+    elif week[day_num] == "Wednesdays":
+        return "Quartas"
+
+    elif week[day_num] == "Thursdays":
+        return "Quintas"
+
+    elif week[day_num] == "Fridays":
+        return "Sextas"
+
+    elif week[day_num] == "Saturdays":
+        return "Sábados"
+
+    else:
+        return "ERRO!"
+
+
+def convert_hours(hours: str) -> str:
+    """ Converte O fuso horario japonês para o brasileiro """
+    time_zone = 12
+
+    j_hours = int(hours[:2])
+    if j_hours >= time_zone:
+        br_hours = str(j_hours - time_zone)
+    else:
+        br_hours = str(j_hours + time_zone)
+
+    return br_hours + hours[2:]
+
+
+def get_day(data: dict) -> dict: # pode fazer a conversão para o dia e horas do Brasil
     """ diz quando o anime com o ID informado lança novos episodeos
     :param - id_mal
     :return - None
     """
-    data = get_request(f"anime/{id_mal}")
+    if int(data["time"][:2]) - 12 <= 0:
+        day  = convert_day(data["day"], True)
+    else:
+        day  = convert_day(data["day"])
+    hours  = convert_hours(data["time"])
 
-    day  = data["data"]["broadcast"]["day"]
-    nome = data["data"]["title"]
-    time  = data["data"]["broadcast"]["time"]
-
-    print(f"\nO anime {nome} lança um episodio novo as {time} de {day}\n")
+    return {"dia": day, "horas": hours}
 
 
 def lista_do_dia(day:str) -> list: # Posso acabar usando muito essa
@@ -45,8 +90,8 @@ def lista_do_dia(day:str) -> list: # Posso acabar usando muito essa
                 anime_dict = {}
                 anime_dict["nome"] = anime["title"]
                 anime_dict["image"] = anime["images"]["jpg"]["image_url"]
+                anime_dict["time"] = get_day(anime["broadcast"])
                 anime_list.append(anime_dict)
-                # anime_list["novo_ep"] = get_day(anime["broadcast"])
 
         current_page += 1
         data = get_request(f"seasons/now?page={current_page}")
